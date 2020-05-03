0aae90ade9ec8a2f7c40e861cc6fbe978e1cec43
Henrique Portela <henrique.portela@gmail.com>
2020-05-03 14:57:55 -0300

    Dashboard: listing products.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,7 +35,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const response = await api.get(`/products`);
+      setProducts(response.data);
     }
 
     loadProducts();
